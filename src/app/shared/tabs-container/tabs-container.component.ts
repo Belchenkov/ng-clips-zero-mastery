0a835e4a44f46308b37fcc79762443fbaec174b6
Алексей Бельченков <u608110@gmail.com>
2022-11-07 19:06:30 +0400

    checkout tabs auth modal

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -13,6 +13,18 @@ export class TabsContainerComponent implements AfterContentInit {
   constructor() { }
 
   ngAfterContentInit(): void {
+    const activeTabs = this.tabs?.filter(tab => tab.active);
+
+    if (!activeTabs || activeTabs.length === 0) {
+      this.selectTab(this.tabs!.first);
+    }
   }
 
+  public selectTab(tab: TabComponent) {
+    this.tabs?.forEach(tab => {
+      tab.active = false;
+    });
+
+    tab.active = true;
+  }
 }
